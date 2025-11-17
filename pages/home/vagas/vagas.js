c7d7d30bf707c3bd728e7332470de56093bc41c1
VicotheGaymer <vinimaxboy@gmail.com>
2025-11-17 15:51:44 -0300

    Update vagas.js

--- pages/home/vagas/vagas.js
+++ pages/home/vagas/vagas.js
@@ -1,14 +1,6 @@
 import { names } from "./variavel.js";
 
 window.names = names;
-
-export function names() {
-    const formu = { email: () => document.getElementById("email") };
-
-    const nome = formu.email().value;
-    console.log(nome);
-    return nome;
-}
 //----------------------------------------------------------------------------------
 
 const link = "https://vagas-fb9c4-default-rtdb.firebaseio.com/";
@@ -214,3 +206,4 @@ function renderSpots() {
 renderSpots();
 
 updateStats();
+
