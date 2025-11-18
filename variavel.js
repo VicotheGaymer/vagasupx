ae053fd33a37e14fdf7f0a6a11caf69eac350d84
VicotheGaymer <vinimaxboy@gmail.com>
2025-11-18 15:19:23 -0300

    Update variavel.js

--- variavel.js
+++ variavel.js
@@ -1,5 +1,6 @@
 const formu = {email: () => document.getElementById("email")}
 const nome = formu.email().value;
 
-sessionStorage.setItem("emaillogado", nome):
-window.location.href = "pages/home/vagas/vaga.html"
+sessionStorage.setItem("emaillogado", nome);
+window.location.href = "pages/home/vagas/vaga.html";
+
